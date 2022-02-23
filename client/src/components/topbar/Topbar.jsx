9d92a0e6d4283bb97463981f54eb10c97c800a15
anju-kumaran <anju.naduth@gmail.com>
2022-02-23 17:37:56 -0500

    Add components to topbar

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -2,8 +2,12 @@ import './topbar.css'
 
 export default function Topbar() {
   return (
-    <div>
-      topbar
+    <div className='topbarContainer'>
+      
+      <div className="topbarleft"></div>
+      <div className="topbarCenter"></div>
+      <div className="topbarRight"></div>
+      
     </div>
   )
 }
