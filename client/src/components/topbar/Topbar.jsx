8a3e6309c1a52803b4cefa521d6573c48674998c
anju-kumaran <anju.naduth@gmail.com>
2022-02-23 17:51:27 -0500

    Add topbar icons using material-ui

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,12 +1,40 @@
-import './topbar.css'
+import './topbar.css';
+import {Search, Person, Chat, Notifications} from '@material-ui/icons';
 
 export default function Topbar() {
   return (
     <div className='topbarContainer'>
       
-      <div className="topbarleft"></div>
-      <div className="topbarCenter"></div>
-      <div className="topbarRight"></div>
+      <div className="topbarleft">
+        <span className="logo">Socially</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <Search/>
+          <input placeholder="Search for friends posts or videos" className="searchInput" />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person/>
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat/>
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications/>
+            <span className="topbarIconBadge">3</span>
+          </div>
+        </div>
+      </div>
       
     </div>
   )
