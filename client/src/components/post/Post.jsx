2d49260859f9b24d58404659ed841c6267e001cd
anju-kumaran <anju.naduth@gmail.com>
2022-03-07 16:03:51 -0500

    Add top left post

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -2,8 +2,19 @@ import './post.css'
 
 export default function Post() {
   return (
-    <div>
-      hellooo
+    <div className='post'>
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img className='postProfileImg' src="/assets/person/1.jpeg" alt="" />
+            <span className="postUsername">Julia Emcy</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight"></div>
+        </div>
+        <div className="postCenter"></div>
+        <div className="postBottom"></div>
+      </div>
     </div>
   )
 }
