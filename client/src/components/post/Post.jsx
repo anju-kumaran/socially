e271c5a0cae8bccbea8a7ee21348f69698b6bc4d
anju-kumaran <anju.naduth@gmail.com>
2022-03-08 14:44:36 -0500

    Add post like icons and like count

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -19,7 +19,14 @@ export default function Post() {
           <span className="postText">Hey It's my first post:)</span>
           <img className='postImg' src="/assets/post/1.jpeg" alt="" />
         </div>
-        <div className="postBottom"></div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className='likeIcon' src="/assets/like.png" alt="" />
+            <img className='likeIcon' src="/assets/heart.png" alt="" />
+            <span className="postLikeCounter">23 people like it</span>
+          </div>
+          <div className="postBottomRight"></div>
+        </div>
       </div>
     </div>
   )
